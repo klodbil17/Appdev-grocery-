09c2067cf670663e5dfca48d85a198dff3e13189
Kementino <24100541@usc.edu.ph>
2025-09-15 19:57:33 +0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -29,7 +29,6 @@ public class Item {
 
     @Override
     public String toString() {
-        return name + " (₱" + price + ") - Stock: " + quantity;
+        return name + " (PHP" + price + ") - Stock: " + quantity;
     }
 }
-
